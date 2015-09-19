64c62968e649933b2db3204ab9fec02c951d0902
Jan Carlo Viray <jancarlo@uniregistry.link>
2015-09-18 19:50:28 -0700

    Removed sudo on shell change and uncommented needed lines

--- provision.sh
+++ provision.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Updating"
-sudo apt-get update
+echo "Updating Apt-Get"
+sudo apt-get update &> /dev/null 2>&1
 
-echo "Upgrading"
+echo "Upgrading Current Packages"
 sudo apt-get upgrade
 
-echo "Installing Required Binaries"
+echo "Installing Required Packages"
 sudo apt-get install -y --fix-missing \
   build-essential \
   software-properties-common \
@@ -14,31 +14,37 @@ sudo apt-get install -y --fix-missing \
   cmake \
   python-dev \
   tmux \
-  curl \
   wget \
   vim-nox \
   supervisor \
+  linux-image-generic-lts-trusty \
+  nginx \
   git \
   zsh \
-  linux-image-generic-lts-trusty \
-  nginx
+  curl &> /dev/null 2>&1
 
-echo "Installing Node Version Manager"
-#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash > /dev/null 2>&1
+echo "Downloading and installing Node Version Manager"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash > /dev/null 2>&1
 
-echo "Installing Docker"
-#curl -sSL https://get.docker.com/ | sh > /dev/null 2>&1
+echo "Downloading and installing Docker"
+curl -sSL https://get.docker.com/ | sh > /dev/null 2>&1
 
-echo "Installing Oh My Zsh"
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" > /dev/null 2>&1
+echo "Downloading and installing Oh My Zsh"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &> /dev/null 2>&1
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="wezm"/g' ~/.zshrc
-sudo chsh -s $(which zsh)
+
+echo "Downloading spf13-vim"
+git clone https://github.com/spf13/spf13-vim &> /dev/null 2>&1
 
 echo "Installing spf13-vim"
-git clone https://github.com/spf13/spf13-vim && sh spf13-vim/bootstrap.sh > /dev/null 2>&1
+sh spf13-vim/bootstrap.sh &> /dev/null 2>&1
 
 # fix vim colors inside tmux
 echo 'export TERM="xterm-256color"' >> ~/.zshrc
 echo 'export TERM="xterm-256color"' >> ~/.bashrc
+
+source ~/.bashrc
+source ~/.zshrc
+chsh -s $(which zsh)
 
 echo "Done"
