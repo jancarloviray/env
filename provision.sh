62560ba90a8635d9828738e3384bbc84a281a09f
Jan Carlo Viray <jancarlo@uniregistry.link>
2015-09-18 19:36:24 -0700

    silent output on provision

--- provision.sh
+++ provision.sh
@@ -24,18 +24,18 @@ sudo apt-get install -y --fix-missing \
   nginx
 
 echo "Installing Node Version Manager"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash > /dev/null 2>&1
 
 echo "Installing Docker"
-curl -sSL https://get.docker.com/ | sh
+#curl -sSL https://get.docker.com/ | sh > /dev/null 2>&1
 
 echo "Installing Oh My Zsh"
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" > /dev/null 2>&1
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="wezm"/g' ~/.zshrc
 sudo chsh -s $(which zsh)
 
 echo "Installing spf13-vim"
-git clone https://github.com/spf13/spf13-vim && sh spf13-vim/bootstrap.sh 
+git clone https://github.com/spf13/spf13-vim && sh spf13-vim/bootstrap.sh > /dev/null 2>&1
 
 # fix vim colors inside tmux
 echo 'export TERM="xterm-256color"' >> ~/.zshrc
