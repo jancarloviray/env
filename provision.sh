62290c2112c0e1e71c3642d02e5cc4fc21595b7d
Jan Carlo Viray <virayjancarlo@live.com>
2015-09-19 14:19:45 -0700

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -3,16 +3,12 @@ echo "Updating Apt-Get"
 sudo apt-get update &> /dev/null 2>&1
 
 echo "Upgrading Current Packages"
-sudo apt-get upgrade -y
+# sudo apt-get upgrade -y
 
 echo "Installing Required Packages"
 sudo apt-get install -y --fix-missing \
-  build-essential \
-  software-properties-common \
-  htop \
   man \
   cmake \
-  python-dev \
   tmux \
   wget \
   vim-nox \
@@ -23,15 +19,15 @@ sudo apt-get install -y --fix-missing \
   zsh \
   curl &> /dev/null 2>&1
 
-echo "Downloading and installing Node Version Manager"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash > /dev/null 2>&1
+# echo "Downloading and installing Node Version Manager"
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash > /dev/null 2>&1
 
-echo "Downloading and installing Docker"
-curl -sSL https://get.docker.com/ | sh > /dev/null 2>&1
+# echo "Downloading and installing Docker"
+# curl -sSL https://get.docker.com/ | sh > /dev/null 2>&1
 
 echo "Downloading and installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &> /dev/null 2>&1
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="pygmalion"/g' ~/.zshrc
+# sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="pygmalion"/g' ~/.zshrc
 
 echo "Downloading spf13-vim"
 git clone https://github.com/spf13/spf13-vim &> /dev/null 2>&1
@@ -42,9 +38,8 @@ sh spf13-vim/bootstrap.sh &> /dev/null 2>&1
 
 # fix vim colors inside tmux
 echo 'export TERM="xterm-256color"' >> $HOME/.zshrc
-echo 'export TERM="xterm-256color"' >> $HOME/.bashrc
 
 sudo chsh -s $(which zsh) vagrant
-vim +BundleInstall! +BundleClean! +q!
+# vim +BundleInstall! +BundleClean! +q!
 
 echo "Done"
