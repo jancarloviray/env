581ba1324d6100b739fd2b0585c9fe20b65036ea
Jan Carlo Viray <jancarlo@uniregistry.link>
2015-09-19 18:17:27 -0700

    Revert "Not using ln because virtual box is slow in mounted shared folders"
    This reverts commit 35d90793253b1dea316e7d20483cdbe820366bdc.

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-lnif() {
-  if [ -e "$1" ]; then
+lnif(){
+  if[ -e "$1" ]; then
     ln -sf "$1" "$2"
   fi
 }
@@ -19,10 +19,12 @@ update_packages(){
   success "Updated apt-get"
 }
 
-# man, tmux, vim-nox
 install_required_packages(){
   sudo apt-get install -y --fix-missing \
+  man \
+  tmux \
   vim \
+  vim-nox \
   git \
   zsh \
   curl &> /dev/null 2>&1
@@ -31,51 +33,26 @@ install_required_packages(){
 
 install_ohmyzsh(){
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &> /dev/null 2>&1
-  cp -R "/vagrant/config/oh-my-zsh/custom" "$HOME/.oh-my-zsh/custom"
-
-  # powerlevel9k theme
-  # sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel9k/powerlevel9k"/g' ~/.zshrc
-  # echo 'POWERLEVEL9K_COLOR_SCHEME="light"' >> $HOME/.zshrc
-  # echo 'POWERLEVEL9K_DISABLE_RPROMPT=true' >> $HOME/.zshrc
-  # echo 'POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir rbenv virtualenv vcs)' >> $HOME/.zshrc
   success "Installed Oh-my-zsh"
+# sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="pygmalion"/g' ~/.zshrc
 }
 
-install_spf13_web(){
+install_spf13(){
   git clone https://github.com/spf13/spf13-vim &> /dev/null 2>&1
-
-  # setup local config
-  lnif "/vagrant/config/vim/.vimrc.bundles.local" "$HOME/.vimrc.bundles.local"
   lnif "/vagrant/config/vim/.vimrc.local" "$HOME/.vimrc.local"
-
+  lnif "/vagrant/config/vim/.vimrc.bundles.local" "$HOME/.vimrc.bundles.local"
   sh spf13-vim/bootstrap.sh &> /dev/null 2>&1
   rm -rf $HOME/spf13-vim
-  success "Installed spf13-vim"
+  success "Install spf13-vim"
 }
 
-install_spf13_local(){
-  # using copy because virtualbox mounted folders are very slow
-
-  cp -R "/vagrant/config/vim/.spf13-vim-3" "$HOME/.spf13-vim-3"
-  cp -R "/vagrant/config/vim/.vim" "$HOME/.vim"
-  cp "/vagrant/config/vim/.vimrc" "$HOME/.vimrc"
-  cp "/vagrant/config/vim/.vimrc.bundles" "$HOME/.vimrc.bundles"
-  cp "/vagrant/config/vim/.vimrc.before" "$HOME/.vimrc.before"
-
-  # setup local config
-  cp "/vagrant/config/vim/.vimrc.bundles.local" "$HOME/.vimrc.bundles.local"
-  cp "/vagrant/config/vim/.vimrc.local" "$HOME/.vimrc.local"
-
-  success "Installed spf13-vim"
-}
-
-update_packages
+update_package 
 install_required_packages
 install_ohmyzsh
-install_spf13_local
-# install_spf13_web
+install_spf13
 
 echo 'export TERM="xterm-256color"' >> $HOME/.zshrc
 sudo chsh -s $(which zsh) vagrant
 
-success "Minimal Setup Done. Run /vagrant/extra.sh to install extra packages."
+success "Minimal Setup Done."
+msg "Execute /vagrant/extra.sh to install the rest of the packages."
