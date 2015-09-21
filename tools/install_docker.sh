38ba0827c8304548818430200276ddf5293cd40b
Jan Carlo Viray <jancarlo@uniregistry.link>
2015-09-21 13:10:32 -0700

    Fixed docker permissions. Also added two docker base image pull

--- tools/install_docker.sh
+++ tools/install_docker.sh
@@ -5,3 +5,10 @@ sudo apt-get update
 
 echo "Downloading and installing Docker"
 curl -sSL https://get.docker.com/ | sh
+
+echo "Create Docker group and add current user"
+sudo usermod -aG docker vagrant
+
+echo "Installing Basic Docker Images"
+docker pull ubuntu
+docker pull debian
